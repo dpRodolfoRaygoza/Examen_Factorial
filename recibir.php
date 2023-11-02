b24eb94c42935a7027e25de685529492da5d4230
unknown <DPORTENIS@DESKTOP-TFIUAE5.grupodp.com.mx>
2023-11-02 14:02:58 -0700

    se agrego funcionalidad factorial

---
+++ recibir.php
@@ -0,0 +1,30 @@
+<?php 
+
+//aqui se recibe el numero que dijita el usuario
+
+$numero= $_POST["numero"];
+echo"El numero capturado es:".$numero;
+echo"<br>";
+$resultado=1;
+$acumulador;
+
+if($numero>0){
+    for( $i=1; $i<=$numero; $i++){
+       
+    
+    echo $numero."X".$i." ";
+     
+    }  
+    
+    echo"<br>";
+
+for( $i=1; $i<=$numero; $i++){
+   
+
+    //echo $numero."X".$i." ";
+  $resultado*=$i; 
+}
+echo "El resultado de la operacion es: ".$resultado;
+}
+
+?>
